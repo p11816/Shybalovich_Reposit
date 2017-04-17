3d69113e9b91fe4e861568f5ace7c815ff2cdb67
jemandast <dim_ast@mail.ru>
2017-04-17 11:36:01 +0300

    Добавил пример Point

---
+++ ManyWindows/ManyWindows/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyWindows.Shapes
+{
+    abstract public class Shape
+    {
+        public int Id { get; private set; }
+
+        static private int lastId = 0;
+
+        public static float Distance(PointF a, PointF b)
+        {
+            return (float)Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+        }
+        protected Shape()
+        {
+            point = new PointF(0, 0);
+            pen = new Pen(Color.Pink);
+            pen.Width = 5;
+            brush = new SolidBrush(Color.Lime);
+            lastId++;
+            Id = lastId;
+        }
+        public abstract void Paint(Graphics g);
+        // положение
+        public PointF point;
+        // контур
+        public Pen pen;
+        // заливка
+        public Brush brush;
+
+        public abstract bool isInside(float p1, float p2);
+    }
+}
---
+++ Provodnik/Provodnik/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Provodnik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttBack = new System.Windows.Forms.Button();
+            this.buttNext = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(284, 262);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(140, 262);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.buttNext);
+            this.panel2.Controls.Add(this.buttBack);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(140, 262);
+            this.panel2.TabIndex = 2;
+            // 
+            // buttBack
+            // 
+            this.buttBack.Location = new System.Drawing.Point(20, 17);
+            this.buttBack.Name = "buttBack";
+            this.buttBack.Size = new System.Drawing.Size(90, 26);
+            this.buttBack.TabIndex = 0;
+            this.buttBack.Text = "Back";
+            this.buttBack.UseVisualStyleBackColor = true;
+            // 
+            // buttNext
+            // 
+            this.buttNext.Location = new System.Drawing.Point(20, 49);
+            this.buttNext.Name = "buttNext";
+            this.buttNext.Size = new System.Drawing.Size(90, 26);
+            this.buttNext.TabIndex = 1;
+            this.buttNext.Text = "button3";
+            this.buttNext.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttNext;
+        private System.Windows.Forms.Button buttBack;
+        private System.Windows.Forms.Button button1;
+    }
+}
+

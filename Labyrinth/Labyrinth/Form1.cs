fa88c1bab6f987583811cc4347ae80d3173dbd85
Apatully <Apatully@gmail.com>
2017-05-10 11:54:09 +0300

    Labirint

---
+++ Labyrinth/Labyrinth/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labyrinth
+{
+    public partial class Form1 : Form
+    {
+        PlaiyingField field;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            field = new PlaiyingField(50, 50);
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // контур
+            Pen pen = new Pen(Color.Black);
+            pen.Width = 1;
+            // заливка
+            Brush brush = new SolidBrush(Color.Black);
+            // коэфициент увеличения
+            int k = 3;
+            // прорисовка поля
+            e.Graphics.DrawRectangle(pen, field.pointField.X, field.pointField.Y, field.col * field.sizeCell * k, field.row * field.sizeCell * k);
+
+            //foreach (var it in field.fieldList)
+            //{
+            //    e.Graphics.FillRectangle(brush, field.pointField.X + it.X * k, field.pointField.Y + it.Y * k, it.Width * k, it.Height * k);
+            //    e.Graphics.DrawRectangle(pen, field.pointField.X + it.X * k, field.pointField.Y + it.Y * k, it.Width * k, it.Height * k);
+            //}
+        }
+
+    }
+}
---
+++ Labyrinth/Labyrinth/PlaiyingField.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace Labyrinth
+{
+    class PlaiyingField
+    {
+        // начальные координаты верхнего левого угла игрового поля
+        public Point pointField;
+        // размер поля
+        public int col;
+        public int row;
+        // размер ячейки
+        public int sizeCell;
+        // высота прямоугольной границы
+        public int hRect;
+        // лист прямоугольных объектов поля
+        public List<Rectangle> fieldList;
+        public PlaiyingField(int x0 = 0, int y0 = 0, int myCol = 20, int myRow = 10, int mySizeCell = 10, int myHRect = 2)
+        {
+            pointField.X = x0;
+            pointField.Y = y0;
+            col = myCol;
+            row = myRow;
+            sizeCell = mySizeCell;
+            hRect = myHRect;
+            fieldList = new List<Rectangle>();
+            createdField();
+        }
+        private void createdField()
+        {
+            // горизонтальные границы
+            for (int i = 0; i < 2; ++i)
+            {
+                for (int j = 0; j < col; ++j)
+                {
+                    fieldList.Add(new Rectangle(j * sizeCell, i * row * sizeCell, sizeCell, hRect));
+                }
+            }
+            // вертикальные границы
+            for (int i = 0; i < 2; ++i)
+            {
+                for (int j = 0; j < row; ++j)
+                {
+                    fieldList.Add(new Rectangle(i * col * sizeCell, j * sizeCell, hRect, sizeCell));
+                }
+            }
+        }
+        // добавляем преграды
+        private void addObstacles()
+        {
+
+        }
+        // добавляем границу
+        private void addBorders()
+        {
+
+        }
+
+    }
+}
